e86f0451d191ac3a410909abee9cdf27ff1649c1
Dorkodu <libre@dorkodu.com>
2021-01-28 12:18:08 +0300

    Add Timer to PerformanceProfiler

--- source/PerformanceProfiler.php
+++ source/PerformanceProfiler.php
@@ -5,7 +5,36 @@
   
   class PerformanceProfiler
   {
-    
+    private int $timePrecision;
+    private int $memoryPrecision;
+    private $timer;
+
+    public function __construct(int $timePrecision = 2, int $memoryPrecision = 2)
+    {
+      $this->timePrecision = $timePrecision;
+      $this->memoryPrecision = $memoryPrecision;
+      $this->timer = new Timer(true);
+    }
+
+    public function start()
+    {
+      $this->timer->start();
+    }
+
+    public function stop()
+    {
+      $this->timer->stop();
+    }
+
+    public function reset()
+    {
+      $this->timer->reset();
+    }
+
+    public function passedTime()
+    {
+      return round($this->timer->passedTime(), $this->timePrecision);
+    }
   }
   
   
