828f8d6a6acef901dfd0ba8353b4728bbb06cffd
Doruk Dorkodu <dorkodu@gmail.com>
2021-04-18 22:22:23 +0300

    Add Seekr

---
+++ source/Dorkodu/Seekr/Seekr.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Dorkodu\Seekr;
+
+use Dorkodu\Seekr\Test\{
+  TestCase,
+  TestResult,
+  TestFunction,
+  TestRepository
+};
+
+use Dorkodu\Seekr\Utils\PerformanceProfiler;
+
+use Dorkodu\Utils\Color;
+use Dorkodu\Utils\Console;
+
+use Closure;
+use Dorkodu\Utils\TerminalUI;
+use ReflectionClass;
+use Exception;
+
+/**
+ * Seekr - a minimalist PHP testing library for writing better tests easily and wisely.
+ * 
+ * @author     Doruk Eray (@dorkodu) <doruk@dorkodu.com>
+ * @copyright  (c) 2021, Doruk Eray
+ * @link       <https://github.com/dorkodu/seekr>
+ * @license    The MIT License (MIT)
+ */
+final class Seekr
+{
+}
+
+
