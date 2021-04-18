444ef5889c7d1c30ca3ab8aa10a98e1aeb62432d
Doruk Dorkodu <dorkodu@gmail.com>
2021-04-18 22:48:48 +0300

    Wrote a lot Constraint helpers

---
+++ source/Dorkodu/Seekr/Constraint.php
@@ -0,0 +1,232 @@
+<?php
+
+namespace Dorkodu\Seekr;
+
+use Exception;
+use ArrayAccess;
+use ReflectionClass;
+use SplObjectStorage;
+use Dorkodu\Utils\Str;
+use ReflectionException;
+
+/**
+ * Constraint class provides useful conditions for easy to use.
+ */
+class Constraint
+{
+  /**
+   * RULE :
+   * - Take parameters and decide if they satisfy the requirements of the constraint
+   */
+
+  /**
+   * Check if this thing equals to your expectation.
+   */
+
+  public static function count(int $expectedCount, $haystack)
+  {
+    return (count($haystack) === $expectedCount);
+  }
+
+  public static function contains($needle, iterable $haystack)
+  {
+    if ($haystack instanceof SplObjectStorage) {
+      return $haystack->contains($needle);
+    }
+
+    foreach ($haystack as $element) {
+      if ($needle === $element) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  public static function stringContains(string $needle, string $haystack)
+  {
+    return Str::contains($haystack, $needle);
+  }
+
+  public static function null($proposedValue)
+  {
+    return is_null($proposedValue);
+  }
+
+  public static function notNull($proposedValue)
+  {
+    return !static::null($proposedValue);
+  }
+
+  public static function empty($thing)
+  {
+    return empty($thing);
+  }
+
+  public static function notEmpty($thing)
+  {
+    return !static::empty($thing);
+  }
+
+  public static function true($thing)
+  {
+    return $thing === true;
+  }
+
+  public static function false($thing)
+  {
+    return $thing === false;
+  }
+
+  # COMPARISONS
+
+  public static function equal($expectation, $thing)
+  {
+    return ($expectation === $thing);
+  }
+
+  public static function notEqual($expectation, $thing)
+  {
+    return ($expectation !== $thing);
+  }
+
+  public static function lessThan($thing, $comparedTo)
+  {
+    return $thing < $comparedTo;
+  }
+
+  public static function lessThanOrEqual($thing, $comparedTo)
+  {
+    return $thing <= $comparedTo;
+  }
+
+  public static function greaterThan($thing, $comparedTo)
+  {
+    return $thing > $comparedTo;
+  }
+
+  public static function greaterThanOrEqual($thing, $comparedTo)
+  {
+    return $thing >= $comparedTo;
+  }
+
+  # TYPE CONSTRAINTS
+
+  public static function bool($thing)
+  {
+    return is_bool($thing);
+  }
+
+  public static function int($thing)
+  {
+    return is_int($thing);
+  }
+
+  public static function float($thing)
+  {
+    return is_float($thing);
+  }
+
+  public static function string($thing)
+  {
+    return is_string($thing);
+  }
+
+  public static function object($thing)
+  {
+    return is_object($thing);
+  }
+
+  public static function callable($thing)
+  {
+    return is_callable($thing);
+  }
+
+  public static function scalar($thing)
+  {
+    return is_scalar($thing);
+  }
+
+  public static function NaN($thing)
+  {
+    return is_nan($thing);
+  }
+
+  public static function numeric($thing)
+  {
+    return is_numeric($thing);
+  }
+
+  public static function resource($thing)
+  {
+    return is_resource($thing);
+  }
+
+  public static function iterable($thing)
+  {
+    return is_iterable($thing);
+  }
+
+  public static function infinite($thing)
+  {
+    return is_infinite($thing);
+  }
+
+  public static function finite($thing)
+  {
+    return is_finite($thing);
+  }
+
+  # RELATIONS
+
+  public static function instanceOf($object, $className)
+  {
+    return ($object instanceof $className) && static::object($object);
+  }
+
+  public static function hasProperty($object, string $propertyName)
+  {
+    try {
+      return (new ReflectionClass($object))->hasProperty($propertyName);
+    } catch (ReflectionException $e) {
+
+      throw new Exception(
+        $e->getMessage(),
+        (int) $e->getCode(),
+        $e
+      );
+    }
+  }
+
+  public static function hasMethod($object, string $propertyName)
+  {
+    try {
+      return (new ReflectionClass($object))->hasMethod($propertyName);
+    } catch (ReflectionException $e) {
+      throw new Exception(
+        $e->getMessage(),
+        (int) $e->getCode(),
+        $e
+      );
+    }
+  }
+
+  public static function hasKey($haystack, $key)
+  {
+    if (is_array($haystack)) {
+      return array_key_exists($haystack, $key);
+    }
+
+    if ($haystack instanceof ArrayAccess) {
+      return $haystack->offsetExists($key);
+    }
+
+    return false;
+  }
+
+
+  public static function hasValue($haystack, $value)
+  {
+    return self::contains($value, $haystack);
+  }
+}
