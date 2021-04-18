d5ac7858943fe19e9deed54508229af919f59fc5
Doruk Dorkodu <dorkodu@gmail.com>
2021-04-18 22:11:28 +0300

    Add props and construct

--- source/Dorkodu/Seekr/Test/TestResult.php
+++ source/Dorkodu/Seekr/Test/TestResult.php
@@ -15,5 +15,35 @@ use ReflectionFunction;
  **/
 final class TestResult
 {
-  
+  /**
+   * @var boolean
+   */
+  private $isSuccess = false;
+
+  /**
+   * @var string
+   */
+  private $output = '';
+
+  /**
+   * @var TestFunction|ReflectionMethod
+   */
+  private $test = null;
+
+  /**
+   * @var TestCase
+   */
+  private $testableInstance = null;
+
+  /**
+   * @var Exception
+   */
+  private $exception = null;
+
+  private $executionTime = null;
+  private $peakMemoryUsage = null;
+
+  private function __construct()
+  {
+  }
 }
