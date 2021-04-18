9dd888314da120f2f90a1182b13afc3c187a5d68
Doruk Dorkodu <dorkodu@gmail.com>
2021-04-18 22:07:35 +0300

    Added props and constructor

--- source/Dorkodu/Seekr/Utils/PerformanceProfiler.php
+++ source/Dorkodu/Seekr/Utils/PerformanceProfiler.php
@@ -6,4 +6,14 @@ use Dorkodu\Utils\Timer;
 
 class PerformanceProfiler
 {
+  private int $timePrecision;
+  private int $memoryPrecision;
+  private $timer;
+
+  public function __construct(int $timePrecision = 2, int $memoryPrecision = 2)
+  {
+    $this->timePrecision = $timePrecision;
+    $this->memoryPrecision = $memoryPrecision;
+    $this->timer = new Timer(true);
+  }
 }
