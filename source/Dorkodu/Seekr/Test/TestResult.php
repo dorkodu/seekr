4d5ed541b1a3680b5a982b20cec0c7acba96d2d8
Doruk Dorkodu <dorkodu@gmail.com>
2021-04-18 22:11:06 +0300

    Add TestResult

---
+++ source/Dorkodu/Seekr/Test/TestResult.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Dorkodu\Seekr\Test;
+
+use Closure;
+use Exception;
+use ReflectionClass;
+use ReflectionMethod;
+use Dorkodu\Utils\Color;
+use Dorkodu\Seekr\Contradiction;
+use ReflectionFunction;
+
+/**
+ * Provides a loggable entity with information on a test and how it executed
+ **/
+final class TestResult
+{
+  
+}
