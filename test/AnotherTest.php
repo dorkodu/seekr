71a54c5ee7fd1044e9abf1ffa857d1c3ebb0edfd
Doruk Dorkodu <dorkodu@gmail.com>
2021-04-22 21:40:39 +0300

    Changed tests

---
+++ test/AnotherTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace SeekrTests;
+
+use Exception;
+use Dorkodu\Seekr\Say;
+use InvalidArgumentException;
+use Dorkodu\Seekr\Test\TestCase;
+
+/**
+ * Another Test class with a few tests
+ */
+class AnotherTest extends TestCase
+{
+  /**
+   * This test will pass
+   */
+  private function testWillPass()
+  {
+    echo "This will pass.";
+  }
+
+  /**
+   * This test will fail
+   */
+  public function testWillFail()
+  {
+    echo "This is the output from a failed test";
+  }
+}
