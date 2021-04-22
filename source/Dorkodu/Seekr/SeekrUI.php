5a55f1e78c2d57435211107f353d8d09ecb5c01b
Doruk Dorkodu <dorkodu@gmail.com>
2021-04-22 13:03:42 +0300

    Added summary to SeekrUI

--- source/Dorkodu/Seekr/SeekrUI.php
+++ source/Dorkodu/Seekr/SeekrUI.php
@@ -23,7 +23,29 @@ class SeekrUI
   {
     Console::breakLine();
     Console::writeLine(
-      Color::colorize("bold, bg-black, fg-white", " .:: Seekr 1.1 ::. ")
+      Color::colorize("bold, bg-black, fg-white", " .:: Seekr 1.1 ")
+        . "by Dorkodu"
+        . Color::colorize("bold, bg-black, fg-white", " ::. ")
+    );
+    Console::breakLine();
+  }
+
+  /**
+   * Prints a summary from the current test results
+   *
+   * @return void
+   */
+  public static function summary(int $successCount, int $failureCount)
+  {
+    Console::breakLine();
+    Console::writeLine(
+      sprintf(
+        Color::colorize("bg-blue, fg-white, bold", " SUMMARY ")
+          . " " . Color::colorize("bold, underlined", "%d") . " passed "
+          . Color::colorize("bold, underlined", "%d") . " failed\n",
+        $successCount,
+        $failureCount
+      )
     );
   }
 }
