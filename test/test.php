cdc2cafde591f0bd78ab2486aa4a196abdf8c74b
Doruk Eray <doruk@dorkodu.com>
2021-08-11 08:37:25 +0300

    Update tests, in test.php

--- test/test.php
+++ test/test.php
@@ -8,22 +8,53 @@ error_reporting(E_ERROR);
  */
 require "loot/loom-weaver.php";
 
-# this is the test file
+// this is the test file
 require_once "SampleTest.php";
 require_once "AnotherTest.php";
 
-# we moved functional tests to another file
-require_once "function-tests.php";
-
 use Dorkodu\Seekr\Seekr;
+use Dorkodu\Seekr\Test\TestFunction;
+use Dorkodu\Seekr\Test\TestRepository;
+
+/**
+ * You can write functional tests by giving a description and a callback 
+ */
+
 use SeekrTests\SampleTest;
 use SeekrTests\AnotherTest;
 
-# You can add a test case class by giving an instance of it.
+// initialize the test repository
+$repo = new TestRepository();
+
+// add test cases
+$repo->case(new SampleTest());
+
+/**
+ * You can also write functional tests 
+ * by giving a description and a callback 
+ */
+$repo->function(
+  "a failing test callback.",
+  function () {
+    echo "This is the output of a failed test.";
+    throw new Exception("This is an exception from a failed test.");
+  }
+);
+
+$repo->function(
+  "a passing test callback.",
+  function () {
+    echo "This test will pass :)";
+  }
+);
+
+Seekr::setRepository($repo);
+
+// You can add a test case class by giving an instance of it.
 Seekr::testCase(new SampleTest());
 Seekr::testCase(new AnotherTest());
 
-# Run Seekr
+// Run Seekr
 Seekr::run([
   'detailed' => 1
 ]);
